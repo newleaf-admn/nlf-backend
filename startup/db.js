54e1d7acb9466fba9f906d30657aa15aef8315cc
Wilslie Dipolog <wils.dipolog@gmail.com>
2023-12-11 19:27:48 +0800

    Updated db.js

--- startup/db.js
+++ startup/db.js
@@ -1,24 +1,22 @@
 const mongoose = require('mongoose');
 
 module.exports = function () {
-  const options = {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-    socketTimeoutMS: 30000,
-    keepAlive: true,
-    dbName: process.env.DB_NAME
-  };
+    const options = {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+        socketTimeoutMS: 30000,
+        keepAlive: true,
+        dbName: process.env.DB_NAME
+    }
 
-  const connectionString = 'mongodb+srv://nwldev:Gd6PVhISTtJZNCoO@ac-j0n5rc2-shard-00-00.9j2yyia.mongodb.net,ac-j0n5rc2-shard-00-01.9j2yyia.mongodb.net,ac-j0n5rc2-shard-00-02.9j2yyia.mongodb.net/test?authSource=admin&replicaSet=atlas-xbmo30-shard-0&retryWrites=true&w=majority';
-
-  mongoose.connect(connectionString, options)
-    .then(() => {
-      console.log('Connected to MongoDB | Server is up and running.');
-    })
-    .catch(error => {
-      console.error('Error connecting to MongoDB:', error);
-    });
-};
-
+    // Updated connection string format
+    mongoose.connect('mongodb+srv://nwldev:Gd6PVhISTtJZNCoO@nwleafdb.9j2yyia.mongodb.net', options)
+        .then(() => {
+            console.log('Connected to MongoDB | Server is up and running.');
+        })
+        .catch(error => {
+            console.log(error);
+        });
+}
