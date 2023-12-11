97ec2c817e7adac2e293486ce6bbc0e2f2559705
Wilslie Dipolog <wils.dipolog@gmail.com>
2023-12-11 16:15:19 +0800

    Updated db.js

--- startup/db.js
+++ startup/db.js
@@ -1,7 +1,6 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 module.exports = function () {
-
   const options = {
     useNewUrlParser: true,
     useCreateIndex: true,
@@ -10,13 +9,13 @@ module.exports = function () {
     socketTimeoutMS: 30000,
     keepAlive: true,
     dbName: process.env.DB_NAME
-  }
+  };
 
   mongoose.connect('mongodb+srv://nwldev:Gd6PVhISTtJZNCoO@nwleafdb.9j2yyia.mongodb.net/?retryWrites=true&w=majority', options)
     .then(() => {
-      console.log('Connected to MongoDB | Server is up and running.')
+      console.log('Connected to MongoDB | Server is up and running.');
     })
     .catch(error => {
-      console.log(error)
-    })
-}
+      console.error('Error connecting to MongoDB:', error);
+    });
+};
